c6f4f230efdff6130d2cfebce1cc8827e154514d
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-02-06 14:39:39 +0800

    update

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const playlistSongs = document.getElementById('playlist-songs')
+const playButton = document.getElementById('play')
+const pauseButton  = document.getElementById('pause')
+
+playlistSongs.addEventListener('click', () => {
+    alert('Great!')
+})
