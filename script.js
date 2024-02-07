c2abef884aa037d88b4dbf4b5532ae101409070b
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-02-07 13:36:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -26,8 +26,50 @@ const allSongs = [
         artist: "Quincy Larson",
         duration: "3:51",
         src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/still-learning.mp3"
+    },
+    {
+        id: 4,
+        title: "Never Not Favored",
+        artist: "Quincy Larson",
+        duration: "3:35",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/never-not-favored.mp3",
+    },
+    {
+        id: 5,
+        title: "From the Ground Up",
+        artist: "Quincy Larson",
+        duration: "3:12",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/from-the-ground-up.mp3",
+    },
+    {
+        id: 6,
+        title: "Walking on Air",
+        artist: "Quincy Larson",
+        duration: "3:25",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/walking-on-air.mp3",
+    },
+    {
+        id: 7,
+        title: "Can't Stop Me. Can't Even Slow Me Down.",
+        artist: "Quincy Larson",
+        duration: "3:52",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/cant-stop-me-cant-even-slow-me-down.mp3",
+    },
+    {
+        id: 8,
+        title: "The Surest Way Out is Through",
+        artist: "Quincy Larson",
+        duration: "3:10",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/the-surest-way-out-is-through.mp3",
+    },
+    {
+        id: 9,
+        title: "Chasing That Feeling",
+        artist: "Quincy Larson",
+        duration: "2:43",
+        src: "https://s3.amazonaws.com/org.freecodecamp.mp3-player-project/chasing-that-feeling.mp3",
     }
-    
+        
 ]
 
 playButton.addEventListener('click', () => {
