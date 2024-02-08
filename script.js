5a24759aa12146948e7026d251274fc5c4392820
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-02-08 16:02:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -109,4 +109,4 @@ playlistSongs.innerHTML = songsHTML
 }
 
 
-renderSongs(userData?.songs.sort()) // optional chaining (?.) helps prevent errors when accessing nested properties that might be null or undefined. For example:
+renderSongs(userData?.songs.sort((a,b) => {})) // optional chaining (?.) helps prevent errors when accessing nested properties that might be null or undefined. For example:
