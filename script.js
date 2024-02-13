3b861d095e5a0ffbec4276bc7a95da1a94b30430
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-02-13 13:23:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -103,7 +103,9 @@ const playSong = (id) => {
     audio.play()
 }
 
-const pauseSong = () => {}
+const pauseSong = () => {
+    userData.songCurrentTime = audio.currentTime
+}
 
 const renderSongs = (array) => {
     // The map() method is used to iterate through an array and return a new array.
