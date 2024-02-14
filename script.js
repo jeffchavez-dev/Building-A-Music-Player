a9f9cfc51499b832ed5cd1327f24f177bc2c68dc
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-02-14 20:49:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -159,6 +159,8 @@ const playPreviousSong = () => {
     
 }
 
+previousButton.addEventListener('click', playPreviousSong)
+
 playButton.addEventListener('click', () => {
     if (userData?.currentSong === null) {
         playSong(userData?.songs[0].id)
