a72528663dc7d192af4ad61335ce243835b8a667
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-02-20 16:07:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -198,6 +198,8 @@ const deleteSong = (id) => {
 }
 
 shuffleButton.addEventListener('click', shuffle)
+
+audio.addEventListener('ended', () => {})
 const setPlayerDisplay = () => {
     const playingSong = document.getElementById("player-song-title")
     const songArtist = document.getElementById("player-song-artist")
