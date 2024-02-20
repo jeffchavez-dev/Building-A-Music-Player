b68d689311ac21982ec92703de9709230eb9e9b8
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-02-20 16:11:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -202,6 +202,9 @@ shuffleButton.addEventListener('click', shuffle)
 audio.addEventListener('ended', () => {
     const currentSongIndex = getCurrentSongIndex()
     const nextSongExists = userData.songs.length - 1 > currentSongIndex
+    if (nextSongExists) {
+        playNextSong()
+    }
 })
 const setPlayerDisplay = () => {
     const playingSong = document.getElementById("player-song-title")
